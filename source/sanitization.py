7bea88ff91aa223c1250215c041247743c713e79
aleksandra0014 <a.walczybok4@gmail.com>
2024-03-11 21:24:45 +0100

    commit

--- source/sanitization.py
+++ source/sanitization.py
@@ -6,12 +6,12 @@ def sanitize(df):
     # Remove rejected applications
     df.drop(df[df['Application_status'] == 'Rejected'].index, inplace=True)
     #remove null loan purpose
-    df.drop(df[df['Loan purpose'].isnull()].index, inplace=True)  # to w sumie już jest w utils
+    df.drop(df[df['Var2'].isnull()].index, inplace=True)  # to w sumie już jest w utils
     # Parse dates
-    df['Application date'] = pd.to_datetime(df['Application date'], format='%d%b%Y %H:%M:%S')
-    df['employment date'] = pd.to_datetime(df['employment date'], format='%d%b%Y', errors='coerce')
+    df['application_date'] = pd.to_datetime(df['application_date'], format='%d%b%Y %H:%M:%S')
+    df['Var13'] = pd.to_datetime(df['Var13'], format='%d%b%Y', errors='coerce')
     # Change Var13 to value representing time of employment in days
-    df['employment date'] = df['Application date'] - df['employment date']
+    df['Var13'] = df['application_date'] - df['Var13']
     df = df.rename(columns={'employment date': 'employment duration'})
 
     return df
