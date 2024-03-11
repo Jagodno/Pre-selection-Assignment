18884fae4499c9e1a8e4404f9447d688d50fd2fb
tymoteuszv <tymekvogt@gmail.com>
2024-03-11 13:49:25 +0100

    add papiez

--- source/sanitization.py
+++ source/sanitization.py
@@ -2,18 +2,23 @@ import pandas as pd
 import regex as re
 import os
 from utils import *
+def sanitize(df):
+    # Remove rejected applications
+    df.drop(df[df['Application_status'] == 'Rejected'].index, inplace=True)
+    #remove null loan purpose
+    df.drop(df[df['Var2'].isnull()].index, inplace=True)
+    # Parse dates
+    df['application_date'] = pd.to_datetime(df['application_date'], format='%d%b%Y %H:%M:%S')
+    df['Var13'] = pd.to_datetime(df['Var13'], format='%d%b%Y', errors='coerce')
+    # Change Var13 to value representing time of employment in days
+    df['Var13'] = df['application_date'] - df['Var13']
+
+    return df
 
 
 if __name__ == '__main__':
     descriptions = get_descriptions('../data/descriptions.txt')
     dev = pd.read_csv('../data/development_sample.csv')
     test = pd.read_csv('../data/testing_sample.csv')
+    dev = sanitize(dev)
     print(dev.head())
-    dev.rename(columns=descriptions, inplace=True)  # Rename columns using the descriptions dictionary
-    print(dev.isnull().sum())
-    print(dev.dtypes)
-    dev_str = dev.select_dtypes(include='object')
-    print(dev_str.head())
-
-def sanitize(df):
-    df.drop(df[df['Application status'] == 'Rejected'].index, inplace=True) # Remove rejected applications
