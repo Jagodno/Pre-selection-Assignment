6148a4126b5c95e25d565de22271c66422b35c3b
Micz26 <122210130+Micz26@users.noreply.github.com>
2024-03-14 20:40:44 +0100

    final commit

--- source/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from source.utils import get_descriptions
-descriptions = get_descriptions('descriptions.txt')
-
-dev = pd.read_csv('development_sample.csv')
-test = pd.read_csv('testing_sample.csv')
-dev.rename(columns=descriptions, inplace=True) # Rename columns using the descriptions dictionary
-
-print(dev.isnull().sum())
